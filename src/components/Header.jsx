e55623bcec8d2033a52823f2c1e08043d2198ea3
Justin Tanyag <jtanyag@gmail.com>
2026-08-06 13:53:17 -0700

    first commit, completed Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./src/assets/globe.png" alt="globe" width="24" height="24" />
+        <p>my travel journal.</p>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
