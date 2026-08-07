438b5afed81cc236fd3f3f57544528fa1dd8e3d3
Justin Tanyag <jtanyag@gmail.com>
2026-08-07 10:16:13 -0700

    Update to use data from array to simulate data from API

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,15 +2,15 @@ function Entry(props) {
   return (
     <article className="entry">
       <div className="entry-image-container">
-        <img src={props.img} alt="Japan" width="1080" height="720" />
+        <img src={props.img.src} alt={props.img.alt} width="1080" height="720" />
       </div>
       <div className="entry-content">
         <div className="entry-header">
           <img src="./src/assets/marker.png" alt="marker" width="21" height="29" />
           <p className="entry-country">{props.country}</p>
-          <a href="{props.mapsLink}" target="_blank">View on Google Maps</a>
+          <a href={props.googleMapsLink} target="_blank">View on Google Maps</a>
         </div>
-        <p className="entry-title">{props.location}</p>
+        <p className="entry-title">{props.title}</p>
         <p className="entry-dates">{props.dates}</p>
         <p className="entry-text">{props.text}</p>
       </div>
